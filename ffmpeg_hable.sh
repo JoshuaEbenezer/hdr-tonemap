82af0b16f1b2f7603d543d7069f07d71271b10cc
Joshua Peter Ebenezer <JoshuaEbenezer@users.noreply.github.com>
2021-04-09 17:10:29 -0500

    Create ffmpeg_hable.sh

---
+++ ffmpeg_hable.sh
@@ -0,0 +1,1 @@
+ffmpeg -i input.mp4 -vf zscale=t=linear:npl=100,format=gbrpf32le,zscale=p=bt709,tonemap=tonemap=hable:desat=0,zscale=t=bt709:m=bt709:r=tv,format=yuv420p -c:v libx265 -crf 0 output.mp4
